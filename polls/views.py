5398bccd5269416ee0dd7faae4bcb11ba9ebaa94
bonot9434 <bonot9434@gmail.com>
2021-11-20 16:26:55 +0900

    url名追加

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
+from django.shortcuts import render
+from .models import Question
 
 # Create your views here.
 def index(request):
@@ -9,7 +12,8 @@ def index(request):
     return render(request,'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = Question.objects.get(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
